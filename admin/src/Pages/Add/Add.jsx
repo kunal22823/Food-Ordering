b50b1a218f2cf6e97e8418df7a75a5acbfb05f9d
Kunal Shinde <113423208+kunal22823@users.noreply.github.com>
2025-06-26 21:28:31 +0530

    Update Add.jsx

--- admin/src/Pages/Add/Add.jsx
+++ admin/src/Pages/Add/Add.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 const Add = () => {
 
-    const url = "http://localhost:4000";
+    const url = "https://food-ordering-backend-nx0w.onrender.com";
     const [image,setImage] = useState(false);
     const [data,setData] = useState({
         name:"",
